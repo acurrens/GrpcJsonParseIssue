2fa86e1158e50dbcc580582bf9b82bcd3948d3d4
Andrew Currens <acurrens3@gmail.com>
2021-02-19 12:52:02 -0600

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using GrpcDemo;
+using System;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Channel chn = new Channel("127.0.0.1:5000",ChannelCredentials.Insecure);
+            var grpcClient = new CarSvc.CarSvcClient(chn);
+            var results = grpcClient.GetAll(new Empty());
+            foreach (var c in results.Cars)
+            {
+                Console.WriteLine($"Id: {c.Id} | Year: {c.Year} | Make: {c.Make} | Model: {c.Model} ");
+            }
+            
+            string newCarJson = @"{""cars"": [
+                    { ""id"":6,""make"":""Nissan"",""model"":""GT-R"",""year"":2017},
+                    { ""id"":7,""make"":""BMW"",""model"":""M3"",""year"":2005, ""engine"":""I6""},
+                    { ""id"":8,""make"":""Chevrolet"",""model"":""Bel Air"",""year"":1957, ""engine"":""V8""}
+                     ]
+                    }";
+
+            var parser = CarList.Parser.WithDiscardUnknownFields(true);
+            var cList = parser.ParseJson(newCarJson);
+
+            foreach (var c in cList.Cars)
+            {
+                Console.WriteLine($"Id: {c.Id} | Year: {c.Year} | Make: {c.Make} | Model: {c.Model} ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcDemo/Services/CarService.cs
@@ -0,0 +1,21 @@
+﻿
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System.Threading.Tasks;
+
+namespace GrpcDemo
+{
+    public class CarService : CarSvc.CarSvcBase
+    {
+        public override Task<CarList> GetAll(Empty request, ServerCallContext context)
+        {
+            var carList = new CarList();
+            carList.Cars.Add(new Car(){ Id = 1, Make = "Mazda", Model = "CX5", Year = 2020 });
+            carList.Cars.Add(new Car(){ Id = 2, Make = "Chevrolet", Model = "Chevelle", Year = 1970 });
+            carList.Cars.Add(new Car(){ Id = 3, Make = "Ford", Model = "Mustang", Year = 1993 });
+            carList.Cars.Add(new Car(){ Id = 4, Make = "Jeep", Model = "CJ7", Year = 1983 });
+            carList.Cars.Add(new Car(){ Id = 5, Make = "Ford", Model = "Bronco", Year = 1978 });
+            return Task.FromResult(carList);
+        }
+    }
+}
